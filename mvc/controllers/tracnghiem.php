77f05251a9cd6f13862b46fed6f20590b26de83a
Nguyễn Văn Trí <91021009+trinv60cntt@users.noreply.github.com>
2022-01-11 20:03:12 +0700

    add time countdown and auto submit when timeout

--- mvc/controllers/tracnghiem.php
+++ mvc/controllers/tracnghiem.php
@@ -19,9 +19,12 @@ class TracNghiem extends Controller{
         for($i = 0; $i < count($listTN); $i++) {
             $listTN[$i]['ListDA'] = json_decode($this->daModel->getDapAns($listTN[$i]['MaCH']), true);
         }
-   
+        
         $kt = json_decode($this->ktModel->getKyThiById($id), true);
         $listMH = json_decode($this->mhModel->listAll(), true);
+        // echo '<pre>';
+        // print_r($kt);
+        // echo '</pre>';
         $this->view("layoutCustomer", [
             'page' => 'indexTracNghiem',
             'listTN' => $listTN,
@@ -32,9 +35,7 @@ class TracNghiem extends Controller{
 
     public function SaveResult() {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // echo '<pre>';
-            // print_r($_POST);
-            // echo '</pre>';
+
             $tongSoCau = 0;
             $soCauDung = 0;
             $soCauSai = 0;
@@ -57,43 +58,6 @@ class TracNghiem extends Controller{
 
             $save = $this->model("KetQuaModel");
             $save->insert($soCauDung, $soCauSai, $soCauChuaChon, $diemSo, $_SESSION["userClient"]["maSV"]);
-            // var_dump($save->insert);
-            // return;
-            // echo 'Số câu đúng: ' . $soCauDung . '<br>';
-            // echo 'Số câu sai: ' . $soCauSai . '<br>';
-            // echo 'Số câu chưa chọn: ' . $soCauChuaChon . '<br>';
-            
-            
-
-            // $listResults = []; 
-            // if (isset($_POST['listDA']) && count($_POST['listDA']) > 0)
-            // {
-            //     foreach ($_POST['listDA'] as $maDA)
-            //     {
-            //         $DA = json_decode($this->daModel->getDapAnById($maDA), true);
-            //         array_push($listResults, $DA[0]);
-            //     }
-            // }
-
-            // $count = 0;
-            // $numQuestions = 0;
-            // foreach ($listResults as $key => $value)
-            // {
-            //     if(isset($value["MaCH"]))
-            //     {
-            //         $numQuestions++;
-            //     }
-            //     if ($value["DungSai"] == '1')
-            //     {
-            //         $count++;
-            //     }
-                
-            // }
-            // echo $numQuestions;
-            // echo $count;
-            // $point = 10 / $numQuestions * $count;
-            // echo 'Tổng số câu đúng:     ' . $count . '/' . $numQuestions. ' Vậy số điểm đạt được là: ' . round($point * 100) / 100;
-        
         }  
         return $this->redirectTo("KetQua", "Index");
     }
--- mvc/views/pages/indexTracNghiem.php
+++ mvc/views/pages/indexTracNghiem.php
@@ -33,16 +33,118 @@
                 echo '</div>';
               }
             }
+            echo '<div id="dom-target" style="display: none;">';
+            $output = $items["ThoiGian"];
+            echo htmlspecialchars($output);
+            echo '</div>';
           }
           ?>
 
         </div>
         <div class="col w-1/3 py-5 ml-32 lg:ml-1">
-          <h1>Thời gian làm bài</h1>
-
-          <input type="submit" value="Nộp bài" class="font-bold text-white bg-blue-500 hover:bg-blue-400 px-3 py-1 rounded-md" />
+          <p>Thời gian làm bài: <span class="timer">a</span></p>
+          <input type="submit" id="clickButton" value="Nộp bài" class="font-bold text-white bg-blue-500 hover:bg-blue-400 px-3 py-1 rounded-md" />
         </div>
       </div>
     </form>
   </section>
 </main>
+
+<script type="text/javascript">
+  class Timer {
+    constructor(root) {
+      root.innerHTML = Timer.getHTML();
+      var div = document.getElementById("dom-target");
+      var myData = div.textContent;
+      console.log(myData);
+      this.el = {
+        minutes: root.querySelector(".timer__part--minutes"),
+        seconds: root.querySelector(".timer__part--seconds"),
+        control: root.querySelector(".timer__btn--control"),
+        reset: root.querySelector(".timer__btn--reset")
+      };
+
+      this.interval = null;
+      this.remainingSeconds = myData * 60;
+      var timeClickButton = (myData * 60)*1000;
+      window.onload = function () {
+            var button = document.getElementById('clickButton');
+            setInterval(function () {
+                button.click();
+            }, timeClickButton); 
+        };
+
+      if (this.interval === null) {
+        this.start();
+      } else {
+        this.stop();
+      }
+
+      this.el.reset.addEventListener("click", () => {
+        const inputMinutes = prompt("Enter number of minutes:");
+
+        if (inputMinutes < 60) {
+          this.stop();
+          this.remainingSeconds = inputMinutes * 60;
+          this.updateInterfaceTime();
+        }
+      });
+    }
+
+    updateInterfaceTime() {
+      const minutes = Math.floor(this.remainingSeconds / 60);
+      const seconds = this.remainingSeconds % 60;
+
+      this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+      this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+    }
+
+    updateInterfaceControls() {
+      if (this.interval === null) {
+        this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+        this.el.control.classList.add("timer__btn--start");
+        this.el.control.classList.remove("timer__btn--stop");
+      } else {
+        this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+        this.el.control.classList.add("timer__btn--stop");
+        this.el.control.classList.remove("timer__btn--start");
+      }
+    }
+
+    start() {
+      if (this.remainingSeconds === 0) return;
+
+      this.interval = setInterval(() => {
+        this.remainingSeconds--;
+        this.updateInterfaceTime();
+
+        if (this.remainingSeconds === 0) {
+          this.stop();
+        }
+      }, 1000);
+
+      this.updateInterfaceControls();
+    }
+
+    stop() {
+      clearInterval(this.interval);
+
+      this.interval = null;
+
+      this.updateInterfaceControls();
+    }
+
+    static getHTML() {
+      return `
+              <span class="timer__part timer__part--minutes">00</span>
+              <span class="timer__part">:</span>
+              <span class="timer__part timer__part--seconds">00</span>
+          `;
+
+    }
+  }
+
+  new Timer(
+    document.querySelector(".timer")
+  );
+</script>
